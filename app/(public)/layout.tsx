77a04f2aba171bd7c3ddda5a0e43c6bd922f4d2f
alijamshidy <kbxxkj@gmail.com>
2025-12-19 02:32:44 +0330

    Refactor layout by updating Providers import path and removing unused Providers and ThemeProvider components to streamline code and enhance maintainability.

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar/Navbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import Providers from "./providers";
+import Providers from "../providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
