d6754903790357f44727c5e02bdf7249c6a35b26
alijamshidy <kbxxkj@gmail.com>
2025-12-18 20:03:54 +0330

    Enhance Nav component with structured layout, including logo, customer links, and search functionality for improved navigation and user experience.

--- components/navbar/Nav.tsx
+++ components/navbar/Nav.tsx
@@ -1,3 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
+import Container from "../global/Container";
+import CustomerLinks from "./CustomerLinks";
+import NavSearch from "./NavSearch";
+
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <>
+      <section>
+        <Container className="flex justify-between sm:flex-row sm:justify-between sm:items-center flex-wrap py-5 gap-4  max-w-full xl:max-w-full">
+          <Link href={"/"}>
+            <Image
+              width={150}
+              height={20}
+              src={`/next.svg`}
+              alt=""
+              priority
+            />
+          </Link>
+          <CustomerLinks />
+        </Container>
+      </section>
+      <section>
+        <Suspense>
+          <NavSearch />
+        </Suspense>
+      </section>
+    </>
+  );
 }
