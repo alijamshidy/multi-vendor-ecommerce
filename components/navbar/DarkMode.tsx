2ecb582dbca4f2a6b4132141a20dddf59c47264c
alijamshidy <kbxxkj@gmail.com>
2025-12-18 20:02:18 +0330

    Update DarkMode component to enhance button styling and improve user interaction with a more intuitive design.

--- components/navbar/DarkMode.tsx
+++ components/navbar/DarkMode.tsx
@@ -15,9 +15,11 @@ export default function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger
+        asChild
+        className="border-none bg-transparent cursor-pointer">
         <Button
-          variant="outline"
+          variant="default"
           size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
