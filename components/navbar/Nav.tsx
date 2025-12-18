6f92bfe017681a0aa90b81a94b00723b968ddc34
alijamshidy <kbxxkj@gmail.com>
2025-12-19 02:33:39 +0330

    Add phone support information and integrate CartButton and WhishlistButton into Navbar for improved user accessibility and navigation.

--- components/navbar/Nav.tsx
+++ components/navbar/Nav.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
+import { FaPhoneAlt } from "react-icons/fa";
 import Container from "../global/Container";
+import { Label } from "../ui/label";
+import CartButton from "./CartButton";
+import CategoriesDropdown from "./CategoriesDropdown";
 import CustomerLinks from "./CustomerLinks";
 import NavSearch from "./NavSearch";
+import WhishlistButton from "./WhishlistButton";
 
 export default function Nav() {
   return (
     <>
       <section>
-        <Container className="flex justify-between sm:flex-row sm:justify-between sm:items-center flex-wrap py-5 gap-4  max-w-full xl:max-w-full">
+        <Container className="flex justify-between sm:flex-row sm:justify-between sm:items-center flex-wrap py-5 gap-4">
           <Link href={"/"}>
             <Image
               width={150}
@@ -20,13 +25,23 @@ export default function Nav() {
             />
           </Link>
           <CustomerLinks />
+          <WhishlistButton />
+          <CartButton />
         </Container>
       </section>
-      <section>
+      <Container className="grid md:grid-cols-4 gap-x-16 items-center">
+        <CategoriesDropdown />
         <Suspense>
           <NavSearch />
         </Suspense>
-      </section>
+        <div className="col-span-1 flex items-center gap-x-3 justify-end">
+          <FaPhoneAlt />
+          <div className="grid gap-y-3">
+            <Label>+1343-43555430</Label>
+            <Label>Support 24/7</Label>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
