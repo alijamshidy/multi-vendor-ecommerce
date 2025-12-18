6402aa6c608032b715b4abde40c7566ab893e91f
alijamshidy <kbxxkj@gmail.com>
2025-12-18 20:01:46 +0330

    Add ContactInfo component to display support email and phone number in the navbar, enhancing user accessibility and communication options.

---
+++ components/navbar/ContactInfo.tsx
@@ -0,0 +1,23 @@
+import { FaMobileAlt } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import Container from "../global/Container";
+import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
+
+export default function ContactInfo() {
+  return (
+    <div className="flex flex-row items-center h-[20px] gap-x-1.5">
+      <Container className="px-0 flex gap-2">
+        <IoMdMail />
+        <Label className="">support@gmail.com</Label>
+      </Container>
+
+      <Separator orientation="vertical" />
+
+      <Container className="px-0 flex gap-2">
+        <FaMobileAlt />
+        <Label className="">+(123)3243343</Label>
+      </Container>
+    </div>
+  );
+}
