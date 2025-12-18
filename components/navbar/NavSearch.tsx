2e26895435137ea5a7256d6205f1c7ef0b7f6ee9
alijamshidy <kbxxkj@gmail.com>
2025-12-19 02:33:31 +0330

    Enhance NavSearch component by adding SelectCategory and Search button, improving user experience and search functionality.

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -2,7 +2,9 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
+import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { SelectCategory } from "./SelectCategory";
 
 export default function NavSearch() {
   const searchParams = useSearchParams();
@@ -17,7 +19,7 @@ export default function NavSearch() {
     } else {
       params.delete("search");
     }
-    replace(`/products?${params.toString()}`);
+    replace(`/?${params.toString()}`);
   }, 500);
   useEffect(() => {
     if (!searchParams.get("search")) {
@@ -25,15 +27,24 @@ export default function NavSearch() {
     }
   }, [searchParams]);
   return (
-    <Input
-      type="search"
-      placeholder="search product..."
-      className="max-w-xs dark:bg-muted"
-      value={search}
-      onChange={e => {
-        setSearch(e.target.value);
-        handleSearch(e.target.value);
-      }}
-    />
+    <div className="col-span-1 md:col-span-2 flex items-center">
+      <SelectCategory />
+      <Input
+        type="search"
+        placeholder="search product..."
+        className="dark:bg-muted rounded-none h-[42px] focus-visible:ring-0 focus-visible:border-gray-400"
+        value={search}
+        onChange={e => {
+          setSearch(e.target.value);
+          handleSearch(e.target.value);
+        }}
+      />
+      <Button
+        variant={"default"}
+        size={"lg"}
+        className="bg-[#059473] hover:bg-[#059473] focus-visible:border-ring focus-visible:ring-[#059473]/50 -ml-px rounded-none h-[41px]">
+        Search
+      </Button>
+    </div>
   );
 }
