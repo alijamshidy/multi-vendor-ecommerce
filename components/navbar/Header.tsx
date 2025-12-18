700b328403db679b6483aecb07ff88449474aa75
alijamshidy <kbxxkj@gmail.com>
2025-12-18 20:04:13 +0330

    Refactor Header component to include ContactInfo and Login link, enhancing user accessibility and navigation options while streamlining layout.

--- components/navbar/Header.tsx
+++ components/navbar/Header.tsx
@@ -1,24 +1,33 @@
-import { Suspense } from "react";
+import Link from "next/link";
+import { FaLock } from "react-icons/fa";
 import Container from "../global/Container";
-import CartButton from "./CartButton";
+import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
+import ContactInfo from "./ContactInfo";
 import DarkMode from "./DarkMode";
 import LangsDropdown from "./LangsDropdown";
-import LinksDropdown from "./LinksDropdown";
-import NavSearch from "./NavSearch";
 
 export default function Header() {
   return (
     <header>
-      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4 bg-blue-300 max-w-full xl:max-w-full">
-        <></>
-        <Suspense>
-          <NavSearch />
-        </Suspense>
-        <div className="flex gap-4 items-center">
-          <CartButton />
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-1 gap-4 bg-blue-300 max-w-full xl:max-w-full">
+        <ContactInfo />
+
+        <div className="flex gap-1 items-center h-[30px] py-1">
           <DarkMode />
+          <Separator orientation="vertical" />
           <LangsDropdown />
-          <LinksDropdown />
+          <Separator orientation="vertical" />
+
+          <Link
+            href={`/login`}
+            className="flex gap-2 ml-2 cursor-pointer">
+            {" "}
+            <FaLock />
+            <Label className="cursor-pointer">Login</Label>
+          </Link>
+
+          {/* <LinksDropdown /> */}
         </div>
       </Container>
     </header>
