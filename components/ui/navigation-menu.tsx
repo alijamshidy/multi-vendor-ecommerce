3a7b17003e62600c5d905076cef1d0a2b2a70d21
alijamshidy <kbxxkj@gmail.com>
2025-12-18 20:03:07 +0330

    Update NavigationMenuTrigger styles to adjust icon rotation behavior, enhancing visual feedback during menu interactions.

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -73,7 +73,7 @@ function NavigationMenuTrigger({
       {...props}>
       {children}{" "}
       <ChevronDownIcon
-        className="relative top-px ml-1 size-3 transition duration-300 group-data-[state=open]:rotate-180"
+        className="relative top-px ml-1 size-3 transition duration-300 group-data-[state=open]:rotate-0 rotate-180"
         aria-hidden="true"
       />
     </NavigationMenuPrimitive.Trigger>
