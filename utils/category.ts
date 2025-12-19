13734dd31563af64f962ce60b5934c1732a01290
alijamshidy <kbxxkj@gmail.com>
2025-12-19 23:49:41 +0330

    Add category type and sample category data to category.ts; update product type structure and sample products in products.ts for improved inventory management and categorization.

---
+++ utils/category.ts
@@ -0,0 +1,86 @@
+type categories = {
+  _id: string;
+  name: string;
+  image: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+};
+export const category: categories[] = [
+  {
+    _id: "67d93812511ba62e1848b18b",
+
+    name: "Mobiles",
+    image:
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1742288902/categorys/qmr8qgcbsz521alt7skd.jpg",
+    slug: "Mobiles",
+    createdAt: "2025-03-18T09:08:34.471Z",
+
+    updatedAt: "2025-03-18T09:08:34.471Z",
+  },
+  {
+    _id: "67d93866511ba62e1848b190",
+
+    name: "Laptops",
+    image:
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1742288987/categorys/rxktnx6hw87rl207x4lg.jpg",
+    slug: "Laptops",
+    createdAt: "2025-03-18T09:09:58.659Z",
+
+    updatedAt: "2025-03-18T09:09:58.659Z",
+  },
+  {
+    _id: "67d938e3511ba62e1848b19b",
+    name: "Speakers",
+    image:
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1742289111/categorys/stmgoyjx40fooujiryzq.png",
+    slug: "Speakers",
+    createdAt: "2025-03-18T09:12:03.542Z",
+
+    updatedAt: "2025-03-18T09:12:03.542Z",
+  },
+  {
+    _id: "67d93961511ba62e1848b19d",
+
+    name: "Top wear",
+    image:
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1742289237/categorys/txqnzkunlkxy4rayculi.jpg",
+    slug: "Top-wear",
+    createdAt: "2025-03-18T09:14:09.118Z",
+
+    updatedAt: "2025-03-18T09:14:09.118Z",
+  },
+  {
+    _id: "67d939aa511ba62e1848b1a2",
+
+    name: "Footwear",
+    image:
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1742289311/categorys/t6r7pikltlyl7zhtvbl0.jpg",
+    slug: "Footwear",
+    createdAt: "2025-03-18T09:15:22.576Z",
+
+    updatedAt: "2025-03-18T09:15:22.576Z",
+  },
+  {
+    _id: "67d939de511ba62e1848b1a7",
+
+    name: "Watches",
+    image:
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1742289362/categorys/yiwnrdemkqxlyvaqsyym.jpg",
+    slug: "Watches",
+    createdAt: "2025-03-18T09:16:14.245Z",
+
+    updatedAt: "2025-03-18T09:16:14.245Z",
+  },
+  {
+    _id: "67d937de511ba62e1848b1a7",
+
+    name: "phone",
+    image:
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1742289237/categorys/txqnzkunlkxy4rayculi.jpg",
+    slug: "Watches",
+    createdAt: "2025-03-18T09:16:14.245Z",
+
+    updatedAt: "2025-03-18T09:16:14.245Z",
+  },
+];
--- utils/products.ts
+++ utils/products.ts
@@ -1,79 +1,112 @@
 export type product = {
-  id: string;
+  _id: string;
   name: string;
-  image: string[];
+  sellerId: string;
+  images: string[] | string;
   price: number;
+  slug: string;
   discount: number;
-  rating: number[];
+  rating: number;
   description: string;
-  quantity: number;
-  review: string[];
+  category: string;
+  stock: number;
+  brand: string;
+  shopName: string;
+  createdAt: string;
+  updatedAt: string;
 };
 export const products: product[] = [
   {
-    id: "04a9n5845hjgf365b48907nfbc897bvc65542",
-    name: "a",
-    image: ["a"],
-    price: 3,
-    discount: 0,
-    rating: [3],
-    description: "lorem ipsum",
-    quantity: 30,
-    review: ["good"],
+    _id: "67f66529bbb26983d2a4cabe",
+    sellerId: "680e4a602bfd973455134e48",
+    name: "iphone 12 pro max",
+    slug: "iphone-12-pro-max",
+    category: "Mobiles",
+    brand: "iphone",
+    price: 1200,
+    stock: 10,
+    discount: 9,
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio repudiandae architecto ipsum. Dolor, sint. Ex sit facilis maxime adipisci perferendis.",
+    shopName: "EasyShop",
+    images: [
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1744200983/products/hvhuczwng8knlqvfwy6e.png",
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1744200984/products/emahrm1etu99sbahr0lw.png",
+    ],
+    rating: 0,
+    createdAt: "2025-04-09T12:16:41.368Z",
+
+    updatedAt: "2025-04-09T12:16:41.368Z",
   },
   {
-    id: "04a95845hjgf365bk48907nfbc897bvc65542",
-    name: "a",
-    image: ["a"],
-    price: 3,
-    discount: 0,
-    rating: [3],
-    description: "lorem ipsum",
-    quantity: 30,
-    review: ["good"],
+    _id: "67f66570bbb26983d2a4cac0",
+
+    sellerId: "680e4a602bfd973455134e48",
+
+    name: "iphone 16",
+    slug: "iphone-16",
+    category: "Mobiles",
+    brand: "iphone",
+    price: 1999,
+    stock: 50,
+    discount: 12,
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, reiciendis adipisci veritatis eveniet eos enim perferendis laborum aliquam dolor. At nihil sed voluptatibus voluptas ipsam illum officiis omnis eaque maxime?",
+    shopName: "EasyShop",
+    images: [
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1744201054/products/yz2dwbubaskn5f8wph8t.png",
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1744201055/products/xbmnhnhit4fb2jffkdl3.png",
+    ],
+    rating: 0,
+    createdAt: "2025-04-09T12:17:52.005Z",
+    updatedAt: "2025-04-09T12:17:52.005Z",
   },
   {
-    id: "04a95845hjgf365b4l8907nfbc897bvc65542",
-    name: "a",
-    image: ["a"],
-    price: 3,
-    discount: 0,
-    rating: [3],
-    description: "lorem ipsum",
-    quantity: 30,
-    review: ["good"],
+    _id: "67f6663cbbb26983d2a4cae4",
+
+    sellerId: "67f6658dbbb26983d2a4cac4",
+
+    name: "speaker 7.1 gaming rapoo",
+    slug: "speaker-7.1-gaming-rapoo",
+    category: "Speakers",
+    brand: "rapoo",
+    price: 500,
+    stock: 5,
+    discount: 6,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, at?",
+    shopName: "EasyShop",
+    images: [
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1744201258/products/rqpw2f54equf3h3n0wxo.png",
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1744201259/products/j5dpuwmelf7hojifspjw.png",
+    ],
+    rating: 0,
+    createdAt: "2025-04-09T12:21:16.191Z",
+
+    updatedAt: "2025-04-09T12:21:16.191Z",
   },
   {
-    id: "04a95845hjgf365b48907nfbc897bvc6542",
-    name: "a",
-    image: ["a"],
-    price: 3,
-    discount: 0,
-    rating: [3],
-    description: "lorem ipsum",
-    quantity: 30,
-    review: ["good"],
-  },
-  {
-    id: "04a95845hjgf365b48907nfbc897bvc6552",
-    name: "a",
-    image: ["a"],
-    price: 3,
-    discount: 0,
-    rating: [3],
-    description: "lorem ipsum",
-    quantity: 30,
-    review: ["good"],
-  },
-  {
-    id: "04a95845hjgf365b48907nfbc897bvc6554",
-    name: "a",
-    image: ["a"],
-    price: 3,
-    discount: 0,
-    rating: [3],
-    description: "lorem ipsum",
-    quantity: 30,
-    review: ["good"],
+    _id: "67f6669bbbb26983d2a4cae6",
+
+    sellerId: "67f6658dbbb26983d2a4cac4",
+
+    name: "tuf f516fx asus gaming",
+    slug: "tuf-f516fx-asus-gaming",
+    category: "Laptops",
+    brand: "asus",
+    price: 1300,
+    stock: 20,
+    discount: 10,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloribus at aut ducimus repellat quod optio cupiditate, nihil amet iste. Maxime recusandae impedit nam, atque iusto explicabo sint? Ut qui nobis et magni aperiam impedit, consectetur omnis ducimus nemo rerum deleniti quas voluptas doloribus, quis accusamus animi numquam, ab veritatis!",
+    shopName: "EasyShop",
+    images: [
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1744201351/products/xeuung5sdkhkdymrwzhj.png",
+      "http://res.cloudinary.com/dqv1sbkvs/image/upload/v1744201354/products/nbyjbanrbohhzy2cpmlw.png",
+    ],
+    rating: 0,
+    createdAt: "2025-04-09T12:22:51.049Z",
+
+    updatedAt: "2025-04-09T12:22:51.049Z",
   },
 ];
