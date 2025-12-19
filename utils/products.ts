28d055a24687255db7166ebc102a3257d5a789e6
alijamshidy <kbxxkj@gmail.com>
2025-12-19 19:53:47 +0330

    Add product type and sample product data to products.ts for inventory management

---
+++ utils/products.ts
@@ -0,0 +1,79 @@
+export type product = {
+  id: string;
+  name: string;
+  image: string[];
+  price: number;
+  discount: number;
+  rating: number[];
+  description: string;
+  quantity: number;
+  review: string[];
+};
+export const products: product[] = [
+  {
+    id: "04a9n5845hjgf365b48907nfbc897bvc65542",
+    name: "a",
+    image: ["a"],
+    price: 3,
+    discount: 0,
+    rating: [3],
+    description: "lorem ipsum",
+    quantity: 30,
+    review: ["good"],
+  },
+  {
+    id: "04a95845hjgf365bk48907nfbc897bvc65542",
+    name: "a",
+    image: ["a"],
+    price: 3,
+    discount: 0,
+    rating: [3],
+    description: "lorem ipsum",
+    quantity: 30,
+    review: ["good"],
+  },
+  {
+    id: "04a95845hjgf365b4l8907nfbc897bvc65542",
+    name: "a",
+    image: ["a"],
+    price: 3,
+    discount: 0,
+    rating: [3],
+    description: "lorem ipsum",
+    quantity: 30,
+    review: ["good"],
+  },
+  {
+    id: "04a95845hjgf365b48907nfbc897bvc6542",
+    name: "a",
+    image: ["a"],
+    price: 3,
+    discount: 0,
+    rating: [3],
+    description: "lorem ipsum",
+    quantity: 30,
+    review: ["good"],
+  },
+  {
+    id: "04a95845hjgf365b48907nfbc897bvc6552",
+    name: "a",
+    image: ["a"],
+    price: 3,
+    discount: 0,
+    rating: [3],
+    description: "lorem ipsum",
+    quantity: 30,
+    review: ["good"],
+  },
+  {
+    id: "04a95845hjgf365b48907nfbc897bvc6554",
+    name: "a",
+    image: ["a"],
+    price: 3,
+    discount: 0,
+    rating: [3],
+    description: "lorem ipsum",
+    quantity: 30,
+    review: ["good"],
+  },
+];
