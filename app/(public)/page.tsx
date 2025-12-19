29eeafea29aa130cb05338123ab9df9cab4114ea
alijamshidy <kbxxkj@gmail.com>
2025-12-19 19:52:43 +0330

    Implement layout for public page by adding Container, Banner, TopCategory, and FeaturedProducts components for improved structure and user experience.

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,3 +1,14 @@
+import Container from "@/components/global/Container";
+import Banner from "@/components/home/Banner";
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import TopCategory from "@/components/home/TopCategory";
+
 export default function page() {
-  return <div>public page</div>;
+  return (
+    <Container className="flex flex-col items-center justify-center">
+      <Banner />
+      <TopCategory />
+      <FeaturedProducts />
+    </Container>
+  );
 }
