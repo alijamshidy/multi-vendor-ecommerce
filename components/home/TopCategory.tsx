4ff51f3ca09ab8f0e42d8569d4b3e70442ee7e85
alijamshidy <kbxxkj@gmail.com>
2025-12-19 23:50:11 +0330

    Refactor TopCategory component to utilize dynamic category data, replacing static items with category images and names linked to their respective pages for enhanced user navigation and visual appeal.

--- components/home/TopCategory.tsx
+++ components/home/TopCategory.tsx
@@ -7,10 +7,12 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { category } from "@/utils/category";
 import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import Link from "next/link";
 import Container from "../global/Container";
 import { Label } from "../ui/label";
-
 export function CarouselSpacing() {
   return (
     <Carousel
@@ -18,19 +20,33 @@ export function CarouselSpacing() {
       plugins={[Autoplay({ delay: 2000, stopOnInteraction: false })]}
       className="w-full">
       <CarouselContent className="-ml-1 h-[130px] 2xl:h-[320px]">
-        {Array.from({ length: 7 }).map((_, index) => (
-          <CarouselItem
-            key={index}
-            className={`pl-1 h-full basis-1/6`}>
-            <div className="p-1 h-full">
-              <Card className="h-full">
-                <CardContent className="flex h-full  items-center justify-center">
-                  <span className="text-2xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
+        {category.map(category => {
+          return (
+            <CarouselItem
+              key={category._id}
+              className={`pl-1 h-full basis-1/6`}>
+              <Link
+                href={`/category/${category.name}`}
+                className="p-1 h-full">
+                <Card className="h-full">
+                  <CardContent className="flex h-full relative items-center justify-center">
+                    <Image
+                      width={200}
+                      height={200}
+                      src={category.image}
+                      alt={category.name}
+                      className="w-full h-full"
+                      priority
+                    />
+                    <span className="text-xl font-semibold absolute bottom-8 bg-gray-400 px-1 py-0.5">
+                      {category.name}
+                    </span>
+                  </CardContent>
+                </Card>
+              </Link>
+            </CarouselItem>
+          );
+        })}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
