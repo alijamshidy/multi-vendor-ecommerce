2de8ed2ebca73924279bab4e2e51cc7e6699b61b
alijamshidy <kbxxkj@gmail.com>
2025-12-23 01:33:45 +0330

    Enhance TopCategory component layout and styling by adjusting carousel item height, updating category links, and adding a separator for improved visual hierarchy.

--- components/home/TopCategory.tsx
+++ components/home/TopCategory.tsx
@@ -13,20 +13,20 @@ import Image from "next/image";
 import Link from "next/link";
 import Container from "../global/Container";
 import { Label } from "../ui/label";
+import { Separator } from "../ui/separator";
 export function CarouselSpacing() {
   return (
     <Carousel
       opts={{ align: "center", loop: true }}
-      plugins={[Autoplay({ delay: 2000, stopOnInteraction: false })]}
-      className="w-full">
-      <CarouselContent className="-ml-1 h-[130px] 2xl:h-[320px]">
+      plugins={[Autoplay({ delay: 2000, stopOnInteraction: false })]}>
+      <CarouselContent className="-ml-1 h-[180px] 2xl:h-[320px]">
         {category.map(category => {
           return (
             <CarouselItem
               key={category._id}
-              className={`pl-1 h-full basis-1/6`}>
+              className={`pl-1 h-[80%] basis-1/6`}>
               <Link
-                href={`/category/${category.name}`}
+                href={`/categories/${category.name}`}
                 className="p-1 h-full">
                 <Card className="h-full">
                   <CardContent className="flex h-full relative items-center justify-center">
@@ -38,7 +38,7 @@ export function CarouselSpacing() {
                       className="w-full h-full"
                       priority
                     />
-                    <span className="text-xl font-semibold absolute bottom-8 bg-gray-400 px-1 py-0.5">
+                    <span className="text-xl font-semibold absolute bottom-4 bg-gray-400/70 px-1 py-0.5">
                       {category.name}
                     </span>
                   </CardContent>
@@ -56,8 +56,12 @@ export function CarouselSpacing() {
 
 export default function TopCategory() {
   return (
-    <Container className="mt-10 mx-auto w-full max-w-full xl:max-w-full px-0 flex flex-col items-center gap-8">
-      <Label>Top Category</Label>
+    <Container className="mt-10 mx-auto w-full max-w-full xl:max-w-full px-0  flex flex-col items-center gap-8">
+      <div className="grid items-center justify-center gap-y-2">
+        <Label className="text-xl md:text-3xl md:font-bold">Top Category</Label>
+        <Separator className="bg-[#059473] data-[orientation=horizontal]:w-[140px] mx-auto" />
+      </div>
+
       <CarouselSpacing />
     </Container>
   );
