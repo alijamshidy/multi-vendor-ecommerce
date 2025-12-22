5be5bc6e5b220c1db8a22817fa655f92d68158e3
alijamshidy <kbxxkj@gmail.com>
2025-12-23 01:34:21 +0330

    Enhance SectionTitle component by adding optional className prop for improved styling flexibility and utilizing utility function for class name management.

--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,9 +1,20 @@
+import { cn } from "@/lib/utils";
 import { Separator } from "../ui/separator";
 
-export default function SectionTitle({ text }: { text: string }) {
+export default function SectionTitle({
+  text,
+  className,
+}: {
+  text: string;
+  className?: string;
+}) {
   return (
     <div>
-      <h2 className="text-3xl font-medium tracking-wider capitalize mb-8">
+      <h2
+        className={cn(
+          "text-3xl font-medium tracking-wider capitalize mb-8",
+          className
+        )}>
         {text}
       </h2>
       <Separator />
