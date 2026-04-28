579e93dafcf33027a0ebff7f74bf60382097ef5d
alijamshidy <kbxxkj@gmail.com>
2026-04-29 01:59:37 +0330

    Resolve merge conflicts in configuration and utility files, updating next.config.ts and package.json for project consistency. Remove unused global types and links, and clean up CSS imports in globals.css. Adjust theme provider implementation for better client-side rendering support.

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 "use client";
-=======
->>>>>>> 872b5acfd427e94fbe17bcfabec14c1342fd95b7
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "next-themes";
 import React from "react";
@@ -12,13 +9,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       <Toaster />
       <ThemeProvider
         attribute={"class"}
-<<<<<<< HEAD
         themes={["light", "dark", "system"]}>
-=======
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange>
->>>>>>> 872b5acfd427e94fbe17bcfabec14c1342fd95b7
         {children}
       </ThemeProvider>
     </>
--- utils/links.ts
+++ utils/links.ts
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 type NavLink = {
-=======
-type Link = {
->>>>>>> 872b5acfd427e94fbe17bcfabec14c1342fd95b7
   href: string;
   label: string;
 };
 
-<<<<<<< HEAD
 export const visitorLinks: NavLink[] = [
   { href: "/", label: "home" },
   { href: "/about", label: "about" },
@@ -30,32 +25,13 @@ export const customerLinks: NavLink[] = [
 ];
 
 export const adminLinks: NavLink[] = [
-=======
-export const links: Link[] = [
-  { href: "/", label: "Home" },
-  { href: "/shop", label: "Shop" },
-  { href: "/blog", label: "products" },
-  { href: "/about_Us", label: "About us" },
-  { href: "/contact_Us", label: "Contact us" },
-];
-
-export const adminLinks: Link[] = [
->>>>>>> 872b5acfd427e94fbe17bcfabec14c1342fd95b7
   { href: "/admin/sales", label: "sales" },
   { href: "/admin/products", label: "my products" },
   { href: "/admin/products/create", label: "create product" },
 ];
 
-<<<<<<< HEAD
 export const sellerLinks: NavLink[] = [
   { href: "/seller/sales", label: "sales" },
   { href: "/seller/products", label: "my products" },
   { href: "/seller/products/create", label: "create product" },
-=======
-export const categorys: Link[] = [
-  { href: "mobile", label: "mobile" },
-  { href: "speaker", label: "speaker" },
-  { href: "television", label: "television" },
-  { href: "smart watch", label: "smart watch" },
->>>>>>> 872b5acfd427e94fbe17bcfabec14c1342fd95b7
 ];
