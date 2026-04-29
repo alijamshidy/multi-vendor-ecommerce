0981cd45477f4bbcda95605ed7be94486be96e5e
alijamshidy <kbxxkj@gmail.com>
2026-04-29 22:51:40 +0330

    Refactor CardButton and WishlistButton components to support localized routing by incorporating GetLocale utility. Remove async keyword from component definitions for improved performance.

--- components/layout/CardButton.tsx
+++ components/layout/CardButton.tsx
@@ -1,9 +1,12 @@
+"use client";
+import { GetLocale } from "@/utils/GetUrlParams";
 import Link from "next/link";
 import { LuShoppingCart } from "react-icons/lu";
 import { Button } from "../ui/button";
 
-export default async function CardButton() {
+export default function CardButton() {
   // const numItemsInCart = await fetchCartItems();
+  const locale = GetLocale();
   const numItemsInCart = 0;
   return (
     <Button
@@ -11,7 +14,7 @@ export default async function CardButton() {
       size={"icon"}
       className="flex justify-center items-center relative">
       <Link
-        href={"/cart"}
+        href={`/${locale}/cart`}
         className="w-full h-full justify-center items-center flex">
         <LuShoppingCart />
         <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
--- components/layout/WishlistButton.tsx
+++ components/layout/WishlistButton.tsx
@@ -1,9 +1,12 @@
+"use client";
+import { GetLocale } from "@/utils/GetUrlParams";
 import Link from "next/link";
 import { LuHeart } from "react-icons/lu";
 import { Button } from "../ui/button";
 
-export default async function WishlistButton() {
+export default function WishlistButton() {
   // const numItemsInCart = await fetchCartItems();
+  const locale = GetLocale();
   const numItemsInCart = 0;
   return (
     <Button
@@ -12,7 +15,7 @@ export default async function WishlistButton() {
       className="flex justify-center items-center relative">
       <Link
         className="w-full h-full justify-center items-center flex"
-        href={"/wishlist"}>
+        href={`/${locale}/wishlist`}>
         <LuHeart />
         <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
           {numItemsInCart}
