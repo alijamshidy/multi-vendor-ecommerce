692bf62052afbc6bb98f1ea929f38645174e2435
alijamshidy <kbxxkj@gmail.com>
2026-05-02 15:42:50 +0330

    Update Products page layout by adding flex properties to the Container component for improved alignment and responsiveness. This change enhances the overall user interface by ensuring consistent item positioning.

--- app/[locale]/(store)/products/page.tsx
+++ app/[locale]/(store)/products/page.tsx
@@ -11,7 +11,7 @@ export default async function Page({
 }) {
   const layout = (await searchParams).layout || "grid";
   return (
-    <Container className="mt-36 w-[90%] md:w-auto ml-[2%] min-w-[calc(95%-16rem)] max-w-[95%] mr-[4.5%] flex">
+    <Container className="mt-36 w-[90%] md:w-auto ml-[2%] min-w-[calc(95%-16rem)] max-w-[95%] mr-[4.5%] flex justify-center items-start">
       <Filter />
       <ProductsPage layout={layout} />
     </Container>
